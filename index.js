c774dd70c1206ec80a28aa3248e590e4c3a45493
Adi Faidz <adifaidzabdullah@gmail.com>
2019-08-26 16:08:14 +0800

    Refactored codes and added a new feature to find all locations within a division of soil resistivity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var map, layer, infoWindow = null, marker = null,
+var map, layer, infoWindow = null, infoWindows = [], marker = null, markers = [],
     regions, locations, lightning_protections, surge_protections;
 var mapOptions = {
     center: {
@@ -10,14 +10,14 @@ var mapOptions = {
 };
 
 var legends = [
-    { label: '1-100', icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png'},
-    { label: '101-200', icon: 'https://maps.google.com/mapfiles/ms/icons/pink-dot.png' },
-    { label: '201-250', icon: 'https://maps.google.com/mapfiles/ms/icons/orange-dot.png' },
-    { label: '251-300', icon: 'https://maps.google.com/mapfiles/ms/icons/yellow-dot.png' },
-    { label: '301-350', icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png' },
-    { label: '351-400', icon: 'https://maps.google.com/mapfiles/ms/icons/ltblue-dot.png' },
-    { label: '401-450', icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png' },
-    { label: '451-500', icon: 'https://maps.google.com/mapfiles/ms/icons/purple-dot.png' }
+    { label: '1-100', min: 1, max: 100, icon: 'red'},
+    { label: '101-200', min: 101, max: 200, icon: 'pink' },
+    { label: '201-250', min: 201, max: 250, icon: 'orange' },
+    { label: '251-300', min: 251,max: 300, icon: 'yellow' },
+    { label: '301-350', min: 301, max: 350, icon: 'green' },
+    { label: '351-400', min: 351, max: 400, icon: 'ltblue' },
+    { label: '401-450', min: 401, max: 460, icon: 'blue' },
+    { label: '451-500', min: 451, max: 500, icon: 'purple' }
 ]
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -60,41 +60,80 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
     document.getElementById('search-button').addEventListener('click', function(){
-        if(infoWindow !== null){
-            google.maps.event.clearInstanceListeners(infoWindow)
-            infoWindow.close()
-            infoWindow = null
-        }
+        clearMap()
+        getMarker()
+    })
+})
 
-        if(marker !== null){
-            google.maps.event.clearInstanceListeners(marker, 'click')
-            marker.setMap(null)
-            marker = null
-        }
-        
-        var loc_id = document.getElementById('location').value,
-            struct_id = document.getElementById('structure').value
-        
-        var location = locations.find(function(location){
-            return location.id == loc_id
-        }),
-            structure = location.zones.find(function(zone){
-            return zone.structure_id == struct_id
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    createPeninsularLayer()
+    createLegend()
+}
+
+function get(name) {
+    return fetch(`data/${name}.json`).then(function (response) {
+        return response.json();
+    })
+}
+
+function createPeninsularLayer(){
+    layer = new google.maps.FusionTablesLayer({
+        map: map,
+        suppressInfoWindows: true,
+        clickable: false,
+        query: {
+            select: "'name_0', 'name_1', 'kml_4326'",
+            from: "420419",
+            where: "'name_0' = 'Malaysia' and 'name_1' not equal to 'Sabah' and 'name_1' not equal to 'Sarawak'"
+        },
+        options: {
+            styleId: 9,
+            templateId: 8
+        },
+    });
+}
+
+function createLegend(){
+    var legendControl = document.getElementById('legend')
+    legends.forEach(function (legend) {
+        var div = document.createElement('div')
+        div.innerHTML = `<div id='${legend.icon}-legend' class="legend-item is-size-7">
+            <img src='https://maps.google.com/mapfiles/ms/icons/${legend.icon}-dot.png'>
+            <label>${legend.label}</label>
+        </div>`
+        legendControl.appendChild(div)
+    })
+
+    var items = document.getElementsByClassName('legend-item')
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', function(){
+            clearMap()
+            getAllMarkers(this.id)
         })
+    }
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend)
+}
 
-        var lps = lightning_protections[structure.lightning_protection - 1].name,
-            sps1 = surge_protections[structure.surge_protection.power -1].name,
-            sps2 = surge_protections[structure.surge_protection.telecom - 1].name,
-            r1_before_class = structure.r1_before >= 1E-5 ? 'code-red' : 'code-green';
-            r1_after_class = structure.r1_after >= 1E-5 ? 'code-red' : 'code-green';
+function getMarker(){
+    var loc_id = document.getElementById('location').value,
+        struct_id = document.getElementById('structure').value
 
-        marker = new google.maps.Marker({
-            position: location.cord,
-            title: "Test",
-            icon: { url : getMarkerColor(location.soil_resistivity)}
-        })
+    var location = locations.find(function (location) {
+        return location.id == loc_id
+    }),
+        structure = location.zones.find(function (zone) {
+        return zone.structure_id == struct_id
+    })
 
-        var contentString = `<div id="content" class="location-info">
+    var lps = lightning_protections[structure.lightning_protection - 1].name,
+        sps1 = surge_protections[structure.surge_protection.power - 1].name,
+        sps2 = surge_protections[structure.surge_protection.telecom - 1].name,
+        r1_before_class = structure.r1_before >= 1E-5 ? 'code-red' : 'code-green';
+    r1_after_class = structure.r1_after >= 1E-5 ? 'code-red' : 'code-green';
+
+    var contentString = `<div id="content" class="location-info">
             <h1 class="head is-size-5 has-text-centered has-text-weight-bold">${location.name}</h1>
             <div class="body">
                 <ul class="info-list">
@@ -120,28 +159,112 @@ document.addEventListener('DOMContentLoaded', function () {
                 </ul>
             </div>
         </div>`;
+    
+    infoWindow = createInfoWindow(contentString)
+    marker = createMarker(location, infoWindow)
+    map.panTo(location.cord)
+}
 
-        infoWindow = new google.maps.InfoWindow({
-            content: contentString
-        })
-        marker.setMap(map)
-        map.panTo(location.cord)
-        marker.addListener('click', function () {
-            infoWindow.open(map, marker)
-        })
+function getAllMarkers(id){
+    var code = id.substr(0, id.indexOf('-'))
+    var legend = legends.find(function(legend){
+        return legend.icon === code
     })
-})
+    var locationList = locations.filter(function(location){
+        return location.soil_resistivity >= legend.min && location.soil_resistivity <= legend.max 
+    })
 
-function get(name) {
-    return fetch(`data/${name}.json`).then(function (response) {
-        return response.json();
+    locationList.forEach(function(location){
+        var contentString = `<div id="content" class="location-info">
+            <h1 class="head is-size-5 has-text-centered has-text-weight-bold">${location.name}</h1>
+            <div class="body">
+                <ul class="info-list">
+                    <li> Lightning Ground Flash Density : ${location.lightning_ground_flash_density}</li>
+                    <li> Soil Resistivity ( Ω meter ) : ${location.soil_resistivity}</li>
+                </ul>
+            </div>
+        </div>`;
+        
+        var iw = createInfoWindow(contentString)
+        var m = createMarker(location, iw)
+
+        markers.push(m)
+        infoWindows.push(iw)
     })
+
+    map.panTo({ lat: 3.9398315, lng: 102.3507851 })
+}
+
+function createInfoWindow(contentString){
+    return new google.maps.InfoWindow({
+        content: contentString
+    })
+}
+
+function createMarker(location, iw){
+    var m =  new google.maps.Marker({
+        position: location.cord,
+        title: location.name,
+        icon: { url: getMarkerColor(location.soil_resistivity) }
+    })
+
+    m.setMap(map)
+    m.addListener('click', function () {
+        if(infoWindows.length > 0){
+            infoWindows.forEach(function(window){
+                window.close()
+            })
+        }
+
+        iw.open(map, m)
+    })
+   return m
+}
+
+function clearMap(){
+    if (infoWindows.length > 0)
+        clearInfoWindows()
+    if (markers.length > 0)
+        clearMarkers()
+    if (infoWindow !== null)
+        clearInfoWindow()
+    if (marker !== null)
+        clearMarker()
+}
+
+function clearInfoWindow() {
+    google.maps.event.clearInstanceListeners(infoWindow)
+    infoWindow.close()
+    infoWindow = null
+}
+
+function clearMarker() {
+    google.maps.event.clearInstanceListeners(marker, 'click')
+    marker.setMap(null)
+    marker = null
+}
+
+function clearInfoWindows() {
+    infoWindows.forEach(function (iw) {
+        google.maps.event.clearInstanceListeners(iw)
+        iw.close()
+    })
+
+    infoWindows = []
+}
+
+function clearMarkers() {
+    markers.forEach(function (m) {
+        google.maps.event.clearInstanceListeners(m, 'click')
+        m.setMap(null)
+    })
+    markers = []
 }
 
 function getMarkerColor(resistivity) {
     var color;
 
-    if(resistivity >= 1 && resistivity <= 50)
+    if (resistivity >= 1 && resistivity <= 50)
         color = 'red'
     else if (resistivity >= 51 && resistivity <= 100)
         color = 'red'
@@ -163,40 +286,4 @@ function getMarkerColor(resistivity) {
         color = 'purple'
 
     return `https://maps.google.com/mapfiles/ms/icons/${color}-dot.png`;
-}
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    layer = new google.maps.FusionTablesLayer({
-        map: map,
-        suppressInfoWindows: true,
-        clickable: false,
-        query: {
-            select: "'name_0', 'name_1', 'kml_4326'",
-            from: "420419",
-            where: "'name_0' = 'Malaysia' and 'name_1' not equal to 'Sabah' and 'name_1' not equal to 'Sarawak'"
-        },
-        options: {
-            styleId: 9,
-            templateId: 8
-        },
-        // styles: [
-        //     {
-        //         polygonOptions: {
-        //             fillColor: '',
-        //             fillOpacity: 0.4,
-        //         }
-        //     }
-        // ]
-    });
-
-    var legendControl = document.getElementById('legend')
-    legends.forEach(function(legend){
-        var div = document.createElement('div')
-        div.innerHTML = `<img src='${legend.icon}'>${legend.label}`
-        legendControl.appendChild(div)
-    })
-
-    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend)
 }
